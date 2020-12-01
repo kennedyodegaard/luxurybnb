2ed982e986c6118f44511eac63bdc13f83e5da3f
Kennedy Odegaard <kennedyaodegaard@gmail.com>
2020-12-01 12:44:22 +0100

    comment to commit

--- db/schema.rb
+++ db/schema.rb
@@ -7,7 +7,7 @@
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
-#
+#hello
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_12_01_101949) do
