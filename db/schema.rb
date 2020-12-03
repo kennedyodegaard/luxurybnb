b13b3474d784c52deb9eb783788d28819eaac91a
Louis Duquesne <louisdqn@gmail.com>
2020-12-03 17:34:56 +0100

    added details on show page and added icons

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,9 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2020_12_03_153754) do
-  
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
