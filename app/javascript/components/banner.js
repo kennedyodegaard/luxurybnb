52616e608a061e733872646dde665aa78a123645
Kennedy Odegaard <kennedyaodegaard@gmail.com>
2020-12-04 16:19:27 +0100

    some last minute style changes

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   if (document.getElementById("title")) {
     new Typed('#title', {
-      strings: ["Go near", "Go luxury"],
+      strings: ["Go near.", "Go luxury."],
       typeSpeed: 50,
       loop: true
     });
