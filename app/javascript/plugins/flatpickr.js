9e216842269fecd9da8c9cf4552ade20559b9f6d
Kennedy Odegaard <kennedyaodegaard@gmail.com>
2020-12-04 11:50:33 +0100

    added flatpickr again

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,15 +1,7 @@
 import flatpickr from "flatpickr";
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {
-    enableTime: true
-  });
-
-  flatpickr("#range_start", {
-    altInput: true,
-    plugins: [new rangePlugin({ input: "#range_end"})]
-  });
+  flatpickr(".datepicker", {});
 };
 
 export { initFlatpickr }
